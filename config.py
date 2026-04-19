4c39e26af8f365a2cf255def6b694d017ce9ad94
Dasa-Alzate <dasa.alzate@gmail.com>
2026-04-19 21:35:50 +0200

    patch: normalize DATABASE_URL scheme to mysql+pymysql

--- config.py
+++ config.py
@@ -3,10 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+_db_url = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:0000@localhost/sunalyze')
+if _db_url.startswith('mysql://'):
+    _db_url = 'mysql+pymysql://' + _db_url[len('mysql://'):]
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-change-in-prod')
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:0000@localhost/sunalyze')
+    SQLALCHEMY_DATABASE_URI = _db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = 'SimpleCache'
     CACHE_DEFAULT_TIMEOUT = 300
